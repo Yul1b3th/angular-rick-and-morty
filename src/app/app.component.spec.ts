1bba151d312b502dbb821f427856a0274655681b
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2024-08-10 18:10:03 +0200

    Changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'rickyMartin' title`, () => {
+  it(`should have the 'RickAndMartin' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rickyMartin');
+    expect(app.title).toEqual('RickAndMartin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rickyMartin');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, RickAndMartin');
   });
 });
